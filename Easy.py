92026bf2185399f655cf2c9770bd0325b6b5c403
William Cheuk Hei Wong <williamwongchurkhay@gmail.com>
2024-10-04 16:50:11 -0400

    Merge arrays

--- Easy.py
+++ Easy.py
@@ -341,3 +341,21 @@ class Solution:
             else:
                 current = current.next  # Move to the next distinct element
         return head
+
+"""
+Merge Sorted Array
+"""
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        i = m + n - 1
+        j = m - 1
+        k = n - 1
+        while k >= 0:
+            if j >= 0 and nums1[j] >= nums2[k]:
+                nums1[i] = nums1[j]
+                nums1[j] = 0
+                j -= 1
+            else:
+                nums1[i] = nums2[k]
+                k -= 1
+            i -= 1
