0d83a4401e1fa13d64ae2a11182c3d81db029f64
William Cheuk Hei Wong <williamwongchurkhay@gmail.com>
2024-10-04 22:05:06 -0400

    Binary inorder

--- Easy.py
+++ Easy.py
@@ -359,3 +359,19 @@ class Solution:
                 nums1[i] = nums2[k]
                 k -= 1
             i -= 1
+
+"""
+Binary Tree Inorder Traversal
+"""
+# Definition for a binary tree node.
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
