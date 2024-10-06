d2a8355d3e533ab1eff7c68e9024a280989797d7
William Cheuk Hei Wong <williamwongchurkhay@gmail.com>
2024-10-05 20:01:55 -0400

    Comare tree

--- Easy.py
+++ Easy.py
@@ -375,3 +375,23 @@ class Solution:
         if root is None:
             return []
         return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
+
+"""
+Same Tree
+"""
+# Definition for a binary tree node.
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # If both nodes are None, trees are the same
+        if not p and not q:
+            return True
+        # If one of the nodes is None, trees are not the same
+        if not p or not q:
+            return False
+        # Check if current node values are the same and recurse on children
+        return (p.val == q.val) and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
