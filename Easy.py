c87fa2d440ea5ac06e62f316caf6d981fb3df770
William Cheuk Hei Wong <williamwongchurkhay@gmail.com>
2024-10-06 21:25:57 -0400

    Symmetry of tree

--- Easy.py
+++ Easy.py
@@ -412,3 +412,25 @@ class Solution:
         left = self.maxDepth(root.left)
         right = self.maxDepth(root.right)
         return left + 1 if left > right else right + 1
+    
+"""
+Symmetric tree
+"""
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def compare(left, right):
+            if not left and not right:
+                return True
+            if not left or not right:
+                return False
+            
+            return (left.val == right.val) and compare(left.left, right.right) and compare(left.right, right.left)
+        return compare(root.left, root.right)
+    
