2fad14eb6b44bbfc1676ff44182225a3c5525786
William Cheuk Hei Wong <williamwongchurkhay@gmail.com>
2024-10-07 22:01:53 -0400

    Build tree

--- Easy.py
+++ Easy.py
@@ -434,3 +434,23 @@ class Solution:
             return (left.val == right.val) and compare(left.left, right.right) and compare(left.right, right.left)
         return compare(root.left, root.right)
     
+"""
+Convert Sorted Array to Binary Search Tree
+"""
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+         def buildTree(left, right):
+             if left > right:
+                 return None
+             mid = (left + right) // 2
+             root = TreeNode(nums[mid])
+             root.left = buildTree(left, mid - 1)
+             root.right = buildTree(mid + 1, right)
+             return root
+         return buildTree(0, len(nums) - 1)
