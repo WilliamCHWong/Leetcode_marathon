f2704a1bf43be164741895291a894c5e38cf270e
William Cheuk Hei Wong <williamwongchurkhay@gmail.com>
2024-10-22 14:02:42 -0400

    reverse integer

--- Medium.py
+++ Medium.py
@@ -73,3 +73,20 @@ class Solution:
         
         # Join all rows into one string
         return ''.join(rows)
+"""
+7. Reverse Integer
+"""  
+class Solution:
+    def reverse(self, x: int) -> int:
+        INT_MAX = 2**31 - 1  # 2147483647
+        
+        sign = 1 if x > 0 else -1
+        x = abs(x)
+        result = 0
+        while x != 0:
+            digit = x % 10
+            x //= 10
+            if result > (INT_MAX - digit) // 10:
+                return 0
+            result = result * 10 + digit
+        return result * sign
