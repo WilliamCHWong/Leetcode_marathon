b44a203cdf2245aa94d1f0ec52078dae58808b4e
William Cheuk Hei Wong <williamwongchurkhay@gmail.com>
2024-11-01 12:34:18 -0400

    Remove nth node

--- Medium_LinkedList.py
+++ Medium_LinkedList.py
@@ -27,3 +27,26 @@ class Solution:
             current.next = ListNode(carry)
         return last_digit.next
     
+"""
+19. Remove Nth Node From End of List
+"""
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # Handle edge case: remove head
+        dummy = ListNode(0)
+        dummy.next = head
+        fast, slow = dummy, dummy
+
+        # Move fast pointer n + 1 steps ahead
+        for _ in range(n + 1):
+            fast = fast.next
+        
+        # Move both pointers till the end
+        while fast:
+            fast = fast.next
+            slow = slow.next
+        
+        # Remove
+        slow.next = slow.next.next
+        
+        return dummy.next
