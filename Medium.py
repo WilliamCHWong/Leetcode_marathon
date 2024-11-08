7dcbafa0a24fa785e11bd7d18f8bbd37dc821ff4
William Cheuk Hei Wong <williamwongchurkhay@gmail.com>
2024-11-08 18:34:12 -0500

    Rotated array

--- Medium.py
+++ Medium.py
@@ -413,3 +413,35 @@ class Solution:
             nums[left], nums[right] = nums[right], nums[left]
             left += 1
             right -= 1
+
+"""
+33. Search in Rotated Sorted Array
+"""
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            # Check if the middle element is the target
+            if nums[mid] == target:
+                return mid
+            
+            # Determine if the left half is sorted
+            if nums[left] <= nums[mid]:
+                # Check if the target is in the left half
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                # Otherwise, the right half must be sorted
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        
+        # Target not found
+        return -1
