fcf42e54e17bf617bec43a98de6fccc338ae5355
William Cheuk Hei Wong <williamwongchurkhay@gmail.com>
2024-11-22 20:29:09 -0500

    Power

--- Medium.py
+++ Medium.py
@@ -723,3 +723,21 @@ def groupAnagrams(strs: List[str]) -> List[List[str]]:
     
     # Return the grouped dicts as a list of lists
     return list(dicts.values())
+
+"""
+50. Pow(x, n)
+"""
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        if n < 0:
+            x = 1 / x
+            n = -n
+
+        # Divide and conquer
+        half = self.myPow(x, n // 2)
+        if n % 2 == 0:
+            return half * half
+        else:
+            return half * half * x
