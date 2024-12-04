27d8801064090e3e136399a24438c53cc823f265
William Cheuk Hei Wong <williamwongchurkhay@gmail.com>
2024-12-03 19:35:17 -0500

    Minimum path sum

--- Medium.py
+++ Medium.py
@@ -950,3 +950,25 @@ class Solution:
                         matrix[i][j] += matrix[i][j - 1]
 
         return matrix[m - 1][n - 1]
+
+"""
+64. Minimum Path Sum
+"""
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    continue
+                elif i == 0: # Only add the left
+                    grid[i][j] += grid[i][j - 1]
+                elif j == 0: # Only add the top
+                    grid[i][j] += grid[i - 1][j]
+                else: # Compare the min path
+                    grid[i][j] += min(grid[i - 1][j], grid[i][j - 1])
+
+        return grid[m - 1][n - 1]
+
