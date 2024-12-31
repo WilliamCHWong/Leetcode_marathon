c4fea686a830c320cb5b170fd8abf94ebcdb8571
William Cheuk Hei Wong <williamwongchurkhay@gmail.com>
2024-12-30 23:34:21 -0500

    Unique binary tree

--- Medium.py
+++ Medium.py
@@ -1328,3 +1328,19 @@ class Solution:
 
         generate(0, 0, [])
         return result
+
+"""
+96. Unique Binary Search Trees
+"""
+
+class Solution:
+    def numTrees(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        # Base case
+        dp[0], dp[1] = 1, 1
+
+        for i in range(2, n + 1):
+            for j in range(1, i + 1):
+                dp[i] += dp[j - 1] * dp[i - j]
+        
+        return dp[n]
